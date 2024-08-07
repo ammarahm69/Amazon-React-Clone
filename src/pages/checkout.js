849b2867033add2928e985f8b488197fef7ce2f6
Ammar Ahmed <ammar69aa59@gmail.com>
2024-08-07 02:56:18 -0700

    total-items

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,8 +2,8 @@ import { useSelector } from "react-redux";
 import { selectItems } from "../slices/basketSlice";
 import CheckoutProduct from "../component/CheckoutProduct";
 function Checkout() {
-  const items = useSelector(selectItems)
-  
+  const items = useSelector(selectItems);
+
   return (
     <div className="bg-gray-100 flex-col m-auto max-w-2xl">
       {/* Left */}
@@ -18,24 +18,35 @@ function Checkout() {
       </div>
       <div className="flex flex-col p-5 space-y-10 bg-white">
         <h1 className="text-3xl border-b pb-4">
-          {items.length === 0 ?"Your Amazon Basket is empty" : "Shopping Basket"}
+          {items.length === 0
+            ? "Your Amazon Basket is empty"
+            : "Shopping Basket"}
         </h1>
 
-       {items.map((items,i)=> (
-        <CheckoutProduct 
-        key={i}
-        id={items.id}
-        title={items.title}
-        rating={items.rating}
-        price={items.price}
-        description={items.description}
-        hasPrime={items.hasPrime}
-        category={items.category}
-        image={items.image}
-        />
-       ))}
+        {items.map((items, i) => (
+          <CheckoutProduct
+            key={i}
+            id={items.id}
+            title={items.title}
+            rating={items.rating}
+            price={items.price}
+            description={items.description}
+            hasPrime={items.hasPrime}
+            category={items.category}
+            image={items.image}
+          />
+        ))}
       </div>
+
       {/*Right  */}
+
+      <div>
+        {items.length > 0 && (
+          <>
+            <h2 className="whitespace-nowrap">({items.length} items)</h2>
+          </>
+        )}
+      </div>
     </div>
   );
 }
